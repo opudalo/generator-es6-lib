03483556224325643a96a656cf59e9dbcee6b849
Eugene Chechurin <e.chechurin@gmail.com>
2015-04-06 15:13:20 -0700

    updated es6-lib version

--- app/templates/test/_test.js
+++ app/templates/test/_test.js
@@ -1,7 +1,5 @@
 import <%= exportName %> from '../src/<%= baseFileName %>'
-import { chai }  from 'es6-lib'
-
-let {expect} = chai
+import { expect }  from 'chai'
 
 describe('<%= libName %> basics', function () {
   it('should exist', function () {
